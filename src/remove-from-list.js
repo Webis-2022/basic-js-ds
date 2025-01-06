3ee8fc847bf1adda265a3cbd970930d4417be31e
Webis-2022 <verjovkin.jevgeni@gmail.com>
2025-01-06 19:23:45 +0200

    feat: add tasks solution

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,65 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+// function createLinkedList(array) {
+//   let dummy = new ListNode(0);
+//   let current = dummy;
+//   array.forEach(value => {
+//     current.next = new ListNode(value);
+//     current = current.next;
+//   });
+//   return dummy.next;
+// }
+
+// function removeKFromList(l, k) {
+//   if (!l) return null;
+
+//   let dummy = new ListNode(0);
+//   dummy.next = l;
+//   let current = dummy;
+
+//   while (current && current.next) {
+//     if (current.next.value === k) {
+//       current.next = current.next.next;
+//     } else {
+//         current = current.next;
+//     }
+//   }
+
+//   return dummy.next;
+// }
+
+// const inputArray = [3, 1, 2, 3, 4, 5];
+// const linkedList = createLinkedList(inputArray);
+// const k = 3;
+
+// const result = removeKFromList(linkedList, k);
+
+// function linkedListToArray(list) {
+//   const result = [];
+//   while (list) {
+//     result.push(list.value);
+//     list = list.next;
+//   }
+//   return result;
+// }
+
+function removeKFromList(l, k) {
+  let dummy = new ListNode(0);
+  dummy.next = l;
+  let current = dummy;
+  console.log(current.next.value);
+
+  while (current !== null && current.next !== null) {
+      if (current.next.value === k) {
+          current.next = current.next.next;
+      } else {
+          current = current.next;
+      }
+  }
+
+  return dummy.next;
 }
 
 module.exports = {
--- src/stack.js
+++ src/stack.js
@@ -13,21 +13,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.stack = [];
   }
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  push(element) {
+    this.stack.push(element);
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.stack.length === 0) {
+      return undefined;
+    }
+    return this.stack[this.stack.length - 1];
+  }
+
+  pop() {
+    return this.stack.pop();
   }
 }
+
 
 module.exports = {
   Stack
