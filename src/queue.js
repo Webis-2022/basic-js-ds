8fee676506db4224b19295edb4e41d8f16352591
Webis-2022 <verjovkin.jevgeni@gmail.com>
2025-11-10 18:13:00 +0200

    feat: complete task again2

--- src/queue.js
+++ src/queue.js
@@ -23,25 +23,27 @@ class Queue {
   }
 
   enqueue(value) {
-    const newNode = new ListNode(value);
-    if(this.tail) {
+    const newNode = { value, next: null };
+    if (this.tail) {
       this.tail.next = newNode;
     }
     this.tail = newNode;
-    if(!this.head) {
+    if (!this.head) {
       this.head = newNode;
     }
   }
 
   dequeue() {
-    if(!this.head) {
-      return null;
+    if (!this.head) {
+      return undefined;
     }
     const value = this.head.value;
     this.head = this.head.next;
-    if(!this.head) {
-      return this.tail = null;
+
+    if (!this.head) {
+      this.tail = null;
     }
+
     return value;
   }
 }
